2a702c10a2535cfc3e5356ad127e2c1411a89e3e
Oningyo <https://github.com/Oningyo/Midterm-homework.git>
2020-05-04 05:20:56 +0800

    環境

---
+++ Colorgame/Assets/Scripts/blue.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class blue : MonoBehaviour
+{
+    public static bool complete;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "兔子")
+        {
+            complete = true;
+        }
+    }
+}
